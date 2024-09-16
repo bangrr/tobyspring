942f5d235281a7041fa13e4b126df2817bd3a556
Mark <qudfbftnqls@naver.com>
2024-09-16 23:59:08 +0900

    Clock을 이용한 시간 테스트

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -1,28 +1,30 @@
 package tobyspring.hellospring.payment;
 
-import org.junit.jupiter.api.DisplayName;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tobyspring.hellospring.TestObjectFactory;
+import tobyspring.hellospring.TestPaymentConfig;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDateTime;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestObjectFactory.class)
+@ContextConfiguration(classes = TestPaymentConfig.class)
 class PaymentServiceSpringTest {
 
     @Autowired PaymentService paymentService;
+    @Autowired Clock clock;
     @Autowired ExRateProviderStub exRateProviderStub;
 
     @Test
-    @DisplayName("prepare 메서드가 요구사항 3가지를 잘 충족했는지 검증")
     void convertedAmount() throws IOException {
         // exRate: 1000
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
@@ -36,9 +38,16 @@ class PaymentServiceSpringTest {
 
         assertThat(payment2.getExRate()).isEqualByComparingTo(valueOf(500));
         assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
+    }
 
-        // 원화환산금액의 유효시간 계산
-//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    @Test
+    void validUntil() throws IOException {
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // valid until의 prepare() 30분 뒤로 설정됐는가?
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
     }
 }
